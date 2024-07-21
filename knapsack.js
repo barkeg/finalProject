6dc793c454669a617e435931b4b40b4aec50b4d9
Greg <barkerg252@gmail.com>
2024-07-21 14:55:10 -0400

    All tests passing

--- knapsack.js
+++ knapsack.js
@@ -1,7 +1,29 @@
 function knapsack(weightCap, weights, values) {
     // Write your code here
-  };
+    let high = 0;
   
+    for(let i = 0; i < weights.length; i++){
+      let cap = weights[i];//3
+      let val = values[i]; //70
+      let newCap = weights[i]; //3
+      let newVal = values[i];
+      for(let j = i + 1; j < weights.length; j++){
+        if(cap < weightCap && cap + weights[j] <= weightCap){
+          cap += weights[j];
+          val += values[j];
+        }else if(newCap + weights[j] <= weightCap){
+          cap = newCap + weights[j];
+          val = newVal + values[j];
+        }
+        if(val > high){
+          high = val;
+        }
+      }
+    }
+  
+    return high;
+  };
+   
   // Use this to test your function:
   const weightCap = 10;
   const weights = [3, 6, 8];
